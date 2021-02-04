9712000b0a2d691ac3d055a09bac8e93e2f40403
李梦龙 <649076408@qq.com>
2021-02-05 00:10:03 +0800

    build($jest): config jest

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    __DEV__: true,
+    __TEST__: true,
+    __VERSION__: require('./package.json').version,
+    __BROWSER__: false,
+    __GLOBAL__: false,
+    __ESM_BUNDLER__: true,
+    __ESM_BROWSER__: false,
+    __NODE_JS__: true,
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'lcov', 'text'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts'
+  ],
+  watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  moduleNameMapper: {
+    waterShared: '<rootDir>/src'
+  },
+  rootDir: __dirname,
+  testMatch: ['<rootDir>/src/__tests__/*.spec.ts'],
+  testPathIgnorePatterns: process.env.SKIP_E2E
+    ? // ignore example tests on netlify builds since they don't contribute
+      // to coverage and can cause netlify builds to fail
+      ['/node_modules/', '/examples/__tests__']
+    : ['/node_modules/']
+}

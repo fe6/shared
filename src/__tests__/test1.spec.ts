248b0522781114a781f82317fe95cfc8def90ec0
李梦龙 <649076408@qq.com>
2021-02-05 10:26:11 +0800

    ci($action): config

--- src/__tests__/test1.spec.ts
+++ src/__tests__/test1.spec.ts
@@ -4,4 +4,8 @@ describe('compiler: parse', () => {
   test('simple text', () => {
     expect(isEmptyObject(1)).toBe(2);
   })
+
+  test('simple text 2', () => {
+    expect(isEmptyObject(-11)).toBe(0);
+  })
 });
